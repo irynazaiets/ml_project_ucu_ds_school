568206a76e04a043d90246e7d4dffe215fd018ab
Ivan Steniakin <ivansteniakin@gmail.com>
2023-10-07 00:10:18 +0300

    added all dataset for 2 classes + metrics

--- ml/histGB.py
+++ ml/histGB.py
@@ -1,49 +1,56 @@
 import json
 import os
 
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, roc_auc_score
 
-labels = ["malware", "non-malware"]
-train = []
-test = []
+# todo: panda library
+benign_folder = 'benign'
+malware_folder = 'malware'
+
+file_process_limit = 1200 # use this variable to control, how many records to process from each file
+
+data = []
 y = []
-y_test = []
 
-for file in os.listdir('ml/benigh')[:-2]:
-    with open(f'ml/benigh/{file}') as file_with_features:
-        train.append(json.load(file_with_features).get("API"))
+for file in os.listdir(f'ml/{benign_folder}'):
+    with open(f'ml/{benign_folder}/{file}') as file_with_features:
+        data.append(json.load(file_with_features).get("API"))
         y.append(1)
+        if list(os.listdir(f'ml/{benign_folder}')).index(file) >= file_process_limit:
+            break
 
-for file in os.listdir('ml/benigh')[:-2]:
-    with open(f'ml/benigh/{file}') as file_with_features:
-            test.append(json.load(file_with_features).get("API"))
-            y_test.append(1)
-
-for file in os.listdir('ml/malware')[:-1]:
-    with open(f'ml/malware/{file}') as file_with_features:
-        train.append(json.load(file_with_features).get("API"))
+for file in os.listdir(f'ml/{malware_folder}'):
+    with open(f'ml/{malware_folder}/{file}') as file_with_features:
+        data.append(json.load(file_with_features).get("API"))
         y.append(-1)
-
-for file in os.listdir('ml/malware')[:-1]:
-    with open(f'ml/malware/{file}') as file_with_features:
-            test.append(json.load(file_with_features).get("API"))
-            y_test.append(-1)
+        if list(os.listdir(f'ml/{malware_folder}')).index(file) >= file_process_limit:
+            break
 
 all_keys = set()
 
-for d in train+test:
+for d in data:
     all_keys.update(d.keys())
 
-for d in train+test:
+for d in data:
     for key in all_keys:
         if key not in d:
             d[key] = None
 
 vec = DictVectorizer()
-tr = vec.fit_transform(train).toarray()
-tst = vec.fit_transform(test).toarray()
+X = vec.fit_transform(data).toarray()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-clf = HistGradientBoostingClassifier().fit(tr, y)
-print(clf.score(tst, y_test))
+clf = HistGradientBoostingClassifier().fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print('Accuracy:', accuracy_score(y_test, y_pred))
+print('F1 Score:', f1_score(y_test, y_pred))
+print('Precision:', precision_score(y_test, y_pred))
+print('Recall:', recall_score(y_test, y_pred))
+print('AUC:', roc_auc_score(y_test, y_pred))
+
+#cross validation
+# 3 class
